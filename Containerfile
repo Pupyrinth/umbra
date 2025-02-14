c4a5bbcf6f705bb198620c25eacdd37a0506a84e
Aethrexal <contact@aethrexal.dev>
2025-02-14 17:27:31 +0100

    KDE

--- Containerfile
+++ Containerfile
@@ -4,8 +4,11 @@ FROM ghcr.io/ublue-os/${BASE_IMAGE}:latest
 COPY build.sh /tmp/build.sh
 COPY scripts/shared/packages.sh /tmp/packages.sh
 
+COPY files/shared /
+
 RUN mkdir -p /var/lib/alternatives && \
     /tmp/build.sh && \
     /tmp/packages.sh && \
+    echo "umbra" >> /usr/share/umbra/image_name && \
     ostree container commit
     
--- scripts/shared/packages.sh
+++ scripts/shared/packages.sh
@@ -7,8 +7,9 @@ echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com
 
 wget -O vesktop.rpm https://vencord.dev/download/vesktop/amd64/rpm
 
-rpm-ostree install virt-manager virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top swtpm edk2-ovmf nvtop btop zsh zoxide fzf java-latest-openjdk.x86_64 code kitty
+rpm-ostree install virt-manager virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top swtpm edk2-ovmf fastfetch nvtop btop zsh zoxide fzf java-latest-openjdk.x86_64 code kitty
 
 rm vesktop.rpm
 
-systemctl enable podman.socket libvirtd
+systemctl enable podman.socket
+systemctl enable libvirtd.service
