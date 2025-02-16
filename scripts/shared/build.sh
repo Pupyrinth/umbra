5736073b44206665213988ddbdf710fbb627d42b
Aethrexal <contact@aethrexal.dev>
2025-02-16 21:03:44 +0100

    [testing] wget

--- scripts/shared/build.sh
+++ scripts/shared/build.sh
@@ -5,6 +5,8 @@ set -ouex pipefail
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo
 
+dnf5 install -y wget
+
 wget https://download.opensuse.org/repositories/home:Alxhr0/Fedora_41/home:Alxhr0.repo -P /etc/yum.repos.d
 
 dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
