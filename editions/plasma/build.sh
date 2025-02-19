9239df07ac9585416a264ecc18ca03d1070fa350
Aethrexal <contact@aethrexal.dev>
2025-02-19 02:01:15 +0100

    [testing] sddm fixing

--- editions/plasma/build.sh
+++ editions/plasma/build.sh
@@ -8,4 +8,5 @@ dnf5 install -y plasma-desktop sddm sddm-kcm flatpak-kcm kde-connect sddm-waylan
 ### Remove packages
 dnf5 remove -y firefox firefox-langpacks
 
+systemctl enable sddm
 systemctl enable sddm-theme-fix.mount
