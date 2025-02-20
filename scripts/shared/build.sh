9a45262a92055714b3eb28dce8b7fb482f66915a
Aethrexal <contact@aethrexal.dev>
2025-02-20 02:13:04 +0100

    [testing] Moved to Nebula

--- scripts/shared/build.sh
+++ scripts/shared/build.sh
@@ -13,7 +13,6 @@ dnf5 install -y wget dnf5-plugins
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo
 dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-wget https://download.opensuse.org/repositories/home:Alxhr0/Fedora_41/home:Alxhr0.repo -P /etc/yum.repos.d
 
 dnf5 copr -y enable sentry/kernel-blu
 
@@ -28,7 +27,7 @@ dnf5 install -y intel-media-driver
 # Kernel BLU
 rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-blu' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
 
-dnf5 install -y virt-manager virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top swtpm edk2-ovmf fastfetch nvtop btop zsh zoxide fzf java-latest-openjdk.x86_64 code kitty flatpak fedora-flathub-remote just nodejs pnpm gh vlc distrobox mangohud hourglass glibc-all-langpacks python3-pip google-noto-fonts-all
+dnf5 install -y virt-manager virt-install libvirt-daemon-config-network libvirt-daemon-kvm qemu-kvm virt-viewer libguestfs-tools python3-libguestfs virt-top swtpm edk2-ovmf nvtop zsh zoxide fzf java-latest-openjdk.x86_64 code kitty flatpak fedora-flathub-remote just nodejs pnpm gh vlc distrobox mangohud hourglass glibc-all-langpacks python3-pip google-noto-fonts-all
 
 dnf5 -y --setopt=install_weak_deps=False install steam gamemode lutris waydroid
 
